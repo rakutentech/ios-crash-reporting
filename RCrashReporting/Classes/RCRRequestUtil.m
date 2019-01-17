c3fc4dbbbd5287c15b104c06a820fafc2dea8268
Smith, Donnie <donnie.smith@rakuten.com>
2019-01-17 15:18:57 +0900

    feat(config): update the config fetch to support v2 of config API. APTT-614
    - get the SDK version string from a podspec build flag
    - add support for Application ID

--- RCrashReporting/Classes/RCRRequestUtil.m
+++ RCrashReporting/Classes/RCRRequestUtil.m
@@ -1,6 +1,15 @@
 #import "RCRDeviceInformation.h"
 #import "RCRRequestUtil.h"
 
+#define RCR_EXPAND_AND_QUOTE0(s) #s
+#define RCR_EXPAND_AND_QUOTE(s) RCR_EXPAND_AND_QUOTE0(s)
+
+#ifndef RCR_SDK_VERSION
+#define RCR_SDK_VERSION 0.0.0
+#endif
+
+NSString* const RCRSDKVersion = @ RCR_EXPAND_AND_QUOTE(RCR_SDK_VERSION);
+
 @implementation RCRRequestUtil
 
 + (NSURLSessionConfiguration*)getSessionConfiguration
@@ -8,7 +17,7 @@
     NSURLSessionConfiguration* sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
     
     // Include subscription key in HTTP header.
-    NSString* subscriptionKey = [RCRRequestUtil getSubscriptionKey];
+    NSString* subscriptionKey = [NSBundle.mainBundle objectForInfoDictionaryKey:@"RPTSubscriptionKey"];
     if (subscriptionKey.length)
     {
         sessionConfig.HTTPAdditionalHeaders = @{@"Ocp-Apim-Subscription-Key":subscriptionKey};
@@ -26,15 +35,6 @@
 + (void)printTroubleshootHelp:(NSException*)exception
 {
     NSLog(@"Unfortunately CrashReporting has had an internal exception. Please contact the RCrashReporting team and attach the following log: %@", exception);
-}
-
-/**
- * Returns the Relay subscription key inside Info.plist.
- */
-+ (NSString*)getSubscriptionKey
-{
-    NSBundle* appBundle = NSBundle.mainBundle;
-    return [appBundle objectForInfoDictionaryKey:@"RPTSubscriptionKey"];
 }
 
 @end
